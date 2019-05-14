516755468c7aad087b57b67f5f97f77d5f60a137
Phyllipe Bezerra <pmba@ic.ufal.br>
2019-05-13 23:56:25 -0300

    creating skills progress bars

--- js/index.js
+++ js/index.js
@@ -1,9 +1,50 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('pmbaAnim'),
-  renderer: 'svg',
-  loop: false,
-  autoplay: true,
-  path: '../anim/pmba.json'
+  renderer : 'svg',
+  loop     : false,
+  autoplay : true,
+  path     : '../anim/pmba.json'
 });
 
-// document.getElementById('pmbaAnim').addEventListener('mouseover', function(){ animation.play(); })
+$('#navbarBrandAnim').mouseenter(() => {
+  animation.setDirection(-1);
+  animation.play(); 
+});
+
+$('#navbarBrandAnim').mouseleave(() => {
+  animation.setDirection(1);
+  animation.play(); 
+});
+
+// document.getElementById('pmbaAnim').addEventListener('mouseout', function() { 
+//   animation.setDirection(1);
+//   animation.play(); 
+// });
+
+$(window).on('scroll', () => {
+  var scrollTop = $(this).scrollTop();
+  var elementTop = $("#aboutMe").position().top; 
+  var elementBottom = elementTop + $("#aboutMe").outerHeight(true);
+  var visible = false;
+  
+  if ((scrollTop >= elementTop && scrollTop <= elementBottom) && !visible) {
+    $(".progress-count").each((i, elem) => {
+      $(elem).animate({
+        "width": $(elem).attr('progress')+"%"
+      }, {
+        duration: 1000
+      });
+    });
+
+    $(".progress-indicator").each((i, elem) => {
+      $(elem).animate({
+        "opacity": "1"
+      }, {
+        duration: 1000
+      });
+    });
+
+    visible = true;
+  } 
+
+});
